dedb705472f56eed9e5ff8d2b15999eb184224ee
Atar Shadmi <atar.shadmi@kaltura.com>
2018-01-02 17:04:35 +0200

    restaurants list

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -1,19 +1,23 @@
 import superagent from 'superagent'
-import * as config from '../config.js';
+import * as config from '../config';
 
 export function listRestaurants(keyword, cb){
-    superagent.post(config.BASE + 'restaurant?keyword=' + keyword)
-        .set('Content-Type', 'application/json')
-        .send()
-        .end((err, res) => {
-            console.log("list restaurants, result:");
-            console.log(res);
-            if(err) {
-                cb(false);
-                return false;
-            } else {
-                cb(res);
-                return true;
-            }
-        })
+    let url = config.BASE + 'restaurant';
+    if (keyword) {
+        url += '?keyword=' + keyword;
+    }
+    superagent.get(url)
+    .set('Content-Type', 'application/json')
+    .send()
+    .end((err, res) => {
+        console.log("list restaurants, result:");
+        console.log(res);
+        if(err) {
+            cb(false);
+            return false;
+        } else {
+            cb(res);
+            return true;
+        }
+    })
 }
