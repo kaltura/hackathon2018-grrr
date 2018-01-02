8c9a271257805f6faf2f340f40b5a3d1d1a84cdc
Roei Azachi <Roei.Azachi@kaltura.com>
2018-01-02 16:24:49 +0200

    where2eat initial commit 1

--- src/components/whereToEat.js
+++ src/components/whereToEat.js
@@ -2,9 +2,38 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Redirect, Link, Switch, HashRouter } from 'react-router-dom';
 
 class WhereToEat extends Component {
+    constructor(props) {
+        super(props);
+
+        var today = new Date(),
+            date = today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear();
+
+        this.state = {
+            date: date
+        };
+        this.options = {
+            values: [
+                { name: 'One', id: 1 },
+                { name: 'Two', id: 2 },
+                { name: 'Three', id: 3 },
+                { name: 'four', id: 4 }
+            ]
+        };
+    }
+
+
+
     render() {
+        let optionTemplate = this.options.values.map(v => (
+            <option value={v.id}>{v.name}</option>
+        ));
         return (
-            <div>where to eat</div>
+            <div where to eat >
+                <label>{this.state.date}</label>
+                <select value={this.options.value} onChange={this.handleChange}>
+                    {optionTemplate}
+                </select>
+            </div>
         )
     }
 }
