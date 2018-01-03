5cc5a8361b20471f1aa10a749e6be81e8997a1be
Gonen Radai <gonen.r@gmail.com>
2018-01-03 20:33:21 +0200

    lat long

--- src/actions/results.js
+++ src/actions/results.js
@@ -2,19 +2,35 @@ import superagent from 'superagent'
 import * as config from '../config.js';
 
 export function whereToEatToday(emails, cb){
-    superagent.get(config.BASE + 'results?users='+emails.join(','))
-    .set('Content-Type', 'application/json')
-    .send()
-    .end((err, res) => {
-      if(err) {
-        cb(false);
-        return false;
-      } else {
-        cb(res.body);
-        return true;
-      }
-    })
+    var location = getLocation((position) => {
+        var url = config.BASE + 'results?users='+emails.join(',');
+        if(position !== false) {
+            url += '&lat='+position.coords.latitude+'&long='+position.coords.longitude+'&distance=0.4';
+        }
+        superagent.get(url)
+        .set('Content-Type', 'application/json')
+        .send()
+        .end((err, res) => {
+          if(err) {
+            cb(false);
+            return false;
+          } else {
+            cb(res.body);
+            return true;
+          }
+        })
+    });
   }
+
+function getLocation(cb) {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            cb(position);
+        });
+    } else { 
+       cb(false);
+    }
+}
 
 export function registerResult(emails, restId, restName, cb){
     superagent.post(config.BASE + 'users/history?users='+emails.join(',') + '&restId=' + restId + '&restName=' + restName)
